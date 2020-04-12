f9e430cea1453795675c175fb52992a66ccd55a9
曾求求 <zengqiuqiu@aiyaapp.com>
2020-04-12 11:40:13 +0800

    构建工程

---
+++ CCNetBleDemo/CCNetBleDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CCNetBleDemo
+//
+//  Created by deqiutseng on 2020/4/12.
+//  Copyright © 2020 deqiutseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CCNetBleDemo/CCNetBleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCNetBleDemo
+//
+//  Created by deqiutseng on 2020/4/12.
+//  Copyright © 2020 deqiutseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
